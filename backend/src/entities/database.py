85e4e7182f0915f3f3de5b1f47d691858411e29b
ashokpkumar <49492598+ashokpkumar@users.noreply.github.com>
2021-04-06 22:57:01 +0530

    added_db
    added_db forproject

---
+++ backend/src/entities/database.py
@@ -0,0 +1,280 @@
+from datetime import datetime
+from sqlalchemy import create_engine, Column, String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import pyodbc
+from sqlalchemy.orm.attributes import QueryableAttribute
+
+#db_url = 'localhost:5432'
+#db_name = 'online-exam'
+#db_user = 'postgres'
+#db_password = '0NLIN3-ex4m'
+#engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
+engine = create_engine("mssql+pyodbc://localhost\SQLEXPRESS/master?driver=SQL Server?Trusted_Connection=yes")
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+
+class employee(Base):
+    __tablename__ = 'employee'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    sur_name = Column(String)
+    initial = Column(String)
+    salutation = Column(String)
+    project_id = Column(String)
+    dept = Column(String)
+
+    emp_start_date = Column(DateTime)
+    emp_last_working_date = Column(DateTime)
+    emp_project_assigned_date = Column(DateTime)
+    emp_project_end_date = Column(DateTime)
+
+    employment_status = Column(String)
+    manager_name = Column(String)
+    manager_dept = Column(String)
+
+    # def __init__(self, created_by):
+    #     self.created_at = datetime.now()
+    #     self.updated_at = datetime.now()
+    #     self.last_updated_by = created_by
+
+    
+    def to_dict(self, show=None, _hide=[], _path=None):
+        """Return a dictionary representation of this model."""
+
+        show = show or []
+
+        hidden = self._hidden_fields if hasattr(self, "_hidden_fields") else []
+        default = self._default_fields if hasattr(self, "_default_fields") else []
+        default.extend(['id', 'modified_at', 'created_at'])
+
+        if not _path:
+            _path = self.__tablename__.lower()
+
+            def prepend_path(item):
+                item = item.lower()
+                if item.split(".", 1)[0] == _path:
+                    return item
+                if len(item) == 0:
+                    return item
+                if item[0] != ".":
+                    item = ".%s" % item
+                item = "%s%s" % (_path, item)
+                return item
+
+            _hide[:] = [prepend_path(x) for x in _hide]
+            show[:] = [prepend_path(x) for x in show]
+
+        columns = self.__table__.columns.keys()
+        relationships = self.__mapper__.relationships.keys()
+        properties = dir(self)
+
+        ret_data = {}
+
+        for key in columns:
+            if key.startswith("_"):
+                continue
+            check = "%s.%s" % (_path, key)
+            if check in _hide or key in hidden:
+                continue
+            if check in show or key in default:
+                ret_data[key] = getattr(self, key)
+
+        for key in relationships:
+            if key.startswith("_"):
+                continue
+            check = "%s.%s" % (_path, key)
+            if check in _hide or key in hidden:
+                continue
+            if check in show or key in default:
+                _hide.append(check)
+                is_list = self.__mapper__.relationships[key].uselist
+                if is_list:
+                    items = getattr(self, key)
+                    if self.__mapper__.relationships[key].query_class is not None:
+                        if hasattr(items, "all"):
+                            items = items.all()
+                    ret_data[key] = []
+                    for item in items:
+                        ret_data[key].append(
+                            item.to_dict(
+                                show=list(show),
+                                _hide=list(_hide),
+                                _path=("%s.%s" % (_path, key.lower())),
+                            )
+                        )
+                else:
+                    if (
+                        self.__mapper__.relationships[key].query_class is not None
+                        or self.__mapper__.relationships[key].instrument_class
+                        is not None
+                    ):
+                        item = getattr(self, key)
+                        if item is not None:
+                            ret_data[key] = item.to_dict(
+                                show=list(show),
+                                _hide=list(_hide),
+                                _path=("%s.%s" % (_path, key.lower())),
+                            )
+                        else:
+                            ret_data[key] = None
+                    else:
+                        ret_data[key] = getattr(self, key)
+
+        for key in list(set(properties) - set(columns) - set(relationships)):
+            if key.startswith("_"):
+                continue
+            if not hasattr(self.__class__, key):
+                continue
+            attr = getattr(self.__class__, key)
+            if not (isinstance(attr, property) or isinstance(attr, QueryableAttribute)):
+                continue
+            check = "%s.%s" % (_path, key)
+            if check in _hide or key in hidden:
+                continue
+            if check in show or key in default:
+                val = getattr(self, key)
+                if hasattr(val, "to_dict"):
+                    ret_data[key] = val.to_dict(
+                        show=list(show),
+                        _hide=list(_hide), _path=("%s.%s" % (_path, key.lower()))
+                        #_path=('%s.%s' % (path, key.lower())),
+                    )
+                else:
+                    try:
+                        ret_data[key] = json.loads(json.dumps(val))
+                    except:
+                        pass
+
+        return ret_data
+
+class project(Base):
+    __tablename__ = 'project'
+    id = Column(Integer, primary_key=True)
+    project_id = Column(String)
+    project_title = Column(String)
+    project_start_date = Column(DateTime)
+    project_status = Column(String)
+
+    def to_dict(self, show=None, _hide=[], _path=None):
+        """Return a dictionary representation of this model."""
+
+        show = show or []
+
+        hidden = self._hidden_fields if hasattr(self, "_hidden_fields") else []
+        default = self._default_fields if hasattr(self, "_default_fields") else []
+        default.extend(['id', 'modified_at', 'created_at'])
+
+        if not _path:
+            _path = self.__tablename__.lower()
+
+            def prepend_path(item):
+                item = item.lower()
+                if item.split(".", 1)[0] == _path:
+                    return item
+                if len(item) == 0:
+                    return item
+                if item[0] != ".":
+                    item = ".%s" % item
+                item = "%s%s" % (_path, item)
+                return item
+
+            _hide[:] = [prepend_path(x) for x in _hide]
+            show[:] = [prepend_path(x) for x in show]
+
+        columns = self.__table__.columns.keys()
+        relationships = self.__mapper__.relationships.keys()
+        properties = dir(self)
+
+        ret_data = {}
+
+        for key in columns:
+            if key.startswith("_"):
+                continue
+            check = "%s.%s" % (_path, key)
+            if check in _hide or key in hidden:
+                continue
+            if check in show or key in default:
+                ret_data[key] = getattr(self, key)
+
+        for key in relationships:
+            if key.startswith("_"):
+                continue
+            check = "%s.%s" % (_path, key)
+            if check in _hide or key in hidden:
+                continue
+            if check in show or key in default:
+                _hide.append(check)
+                is_list = self.__mapper__.relationships[key].uselist
+                if is_list:
+                    items = getattr(self, key)
+                    if self.__mapper__.relationships[key].query_class is not None:
+                        if hasattr(items, "all"):
+                            items = items.all()
+                    ret_data[key] = []
+                    for item in items:
+                        ret_data[key].append(
+                            item.to_dict(
+                                show=list(show),
+                                _hide=list(_hide),
+                                _path=("%s.%s" % (_path, key.lower())),
+                            )
+                        )
+                else:
+                    if (
+                        self.__mapper__.relationships[key].query_class is not None
+                        or self.__mapper__.relationships[key].instrument_class
+                        is not None
+                    ):
+                        item = getattr(self, key)
+                        if item is not None:
+                            ret_data[key] = item.to_dict(
+                                show=list(show),
+                                _hide=list(_hide),
+                                _path=("%s.%s" % (_path, key.lower())),
+                            )
+                        else:
+                            ret_data[key] = None
+                    else:
+                        ret_data[key] = getattr(self, key)
+
+        for key in list(set(properties) - set(columns) - set(relationships)):
+            if key.startswith("_"):
+                continue
+            if not hasattr(self.__class__, key):
+                continue
+            attr = getattr(self.__class__, key)
+            if not (isinstance(attr, property) or isinstance(attr, QueryableAttribute)):
+                continue
+            check = "%s.%s" % (_path, key)
+            if check in _hide or key in hidden:
+                continue
+            if check in show or key in default:
+                val = getattr(self, key)
+                if hasattr(val, "to_dict"):
+                    ret_data[key] = val.to_dict(
+                        show=list(show),
+                        _hide=list(_hide), _path=("%s.%s" % (_path, key.lower()))
+                        #_path=('%s.%s' % (path, key.lower())),
+                    )
+                else:
+                    try:
+                        ret_data[key] = json.loads(json.dumps(val))
+                    except:
+                        pass
+
+        return ret_data
+    #serialize_only = ('id', 'email_id', 'role_type', 'users.id')
+    # _default_fields = [
+    #     "title",
+    #     "description",
+    # ]
+    # _hidden_fields = []
+    # _readonly_fields = []
+
+    # def __init__(self, title, description, created_by):
+    #     Entity.__init__(self, created_by)
+    #     self.title = title
+    #     self.description = description
--- backend/src/main.py
+++ backend/src/main.py
@@ -4,11 +4,14 @@
 #https://medium.com/@anushkamehra16/connecting-to-sql-database-using-sqlalchemy-in-python-2be2cf883f85
 #https://medium.com/@alanhamlett/part-1-sqlalchemy-models-to-json-de398bc2ef47#:~:text=To%20add%20a%20serialization%20method,columns%20and%20returns%20a%20dictionary.&text=def%20to_dict(self%2C%20show%3D,of%20this%20model.%22%22%22
 #sources
-from entities.exam import Exam,ExamSchema
-from entities.entity import Session, engine, Base
+
+#from entities.exam import Exam,ExamSchema
+from entities.database import employee,project
+from entities.database import Session, engine, Base
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+from datetime import time
+import datetime
 app = Flask(__name__)
 CORS(app)
 from sqlalchemy.ext.serializer import loads, dumps
@@ -36,24 +39,64 @@ Base.metadata.create_all(engine)
 # for exam in exams:
 #     print(f'({exam.id}) {exam.title} - {exam.description}')
 
-@app.route('/exams')
-def get_exams():
+@app.route('/employees')
+def employees():
     session = Session()
-    exam_objects = session.query(Exam).all()
-    serialized_obj = [{"title":obj.title,"description":obj.description} for obj in exam_objects]
+    emp_objects = session.query(employee).all()
+    serialized_obj = [{"title":obj.title,"description":obj.description} for obj in emp_objects]
     session.close()
     return (jsonify(serialized_obj))
 
-
-@app.route('/exams', methods=['POST'])
-def add_exam():
-    data = request.get_json()
-    posted_exam = Exam(title=data["title"] , description = data["description"], created_by= "Post request")
+@app.route('/projects')
+def projects():
     session = Session()
-    session.add(posted_exam)
+    project_objects = session.query(project).all()
+    serialized_obj = [{"title":obj.title,"description":obj.description} for obj in project_objects]
+    session.close()
+    return (jsonify(serialized_obj))
+
+@app.route('/addEmployee', methods=['POST'])
+def addEmployee():
+    data = request.get_json()
+    emp_data = employee(first_name=data["first_name"] , 
+                        last_name = data["last_name"], 
+                        sur_name= data["sur_name"], 
+                        initial= data["initial"], 
+                        salutation= data["salutation"], 
+                        project_id= data["project_id"], 
+                        dept= data["dept"], 
+
+                        emp_start_date= datetime.datetime(),
+                        emp_last_working_date=datetime.datetime(),
+                        emp_project_assigned_date=datetime.datetime(),
+                        emp_project_end_date=datetime.datetime(),
+
+                        employment_status=data["employment_status"], 
+                        manager_name=data["manager_name"], 
+                        manager_dept=data["manager_dept"], 
+
+                        )
+    session = Session()
+    session.add(emp_data)
     session.commit()
-    posted_exam.to_dict()
-    return jsonify(posted_exam.to_dict()), 201
+    emp_data.to_dict()
+    return jsonify(emp_data.to_dict()), 201
+
+
+@app.route('/addProject', methods=['POST'])
+def addProject():
+    data = request.get_json()
+    project_data = project(project_id=data["project_id"] , 
+                        project_title = data["project_title"], 
+                        project_start_date= datetime.datetime.now(),
+                        project_status= data["status"], 
+                        
+                        )
+    session = Session()
+    session.add(project_data)
+    session.commit()
+    project_data.to_dict()
+    return jsonify(project_data.to_dict()), 201
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0',port = 5000,debug = True)
